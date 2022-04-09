cf535e2024a26c43fdda8a956f990ec687dbe6d7
Thangvu181864 <thang181864@nuce.edu.vn>
2022-04-09 08:17:28 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://api-nodejs-tutorial.herokuapp.com/",
   headers: {
     "content-type": "application/json",
   },
